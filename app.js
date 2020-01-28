7cc57f35f7cbeaaa04ebee34882f7167dab06ba7
Mariya Lucas <mariya.podosinova@gmail.com>
2020-01-28 15:16:50 +0000

    Setup new campground POST route. Add in body-parser

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
@@ -25,6 +27,10 @@ app.get("/campgrounds", function(req, res) {
     }
   ];
   res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.post("/campgrounds", function(req, res) {
+  res.send("You hit the POST route!");
 });
 
 app.listen(process.env.PORT || 3000, function() {
