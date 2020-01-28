2b2738a8b2abf2b3ac7b2f44ee37fb795937d042
Mariya Lucas <mariya.podosinova@gmail.com>
2020-01-28 15:34:31 +0000

    Add basic nstyled form. Creating Campgrounds

--- app.js
+++ app.js
@@ -9,28 +9,33 @@ app.get("/", function(req, res) {
   res.render("landing");
 });
 
+var campgrounds = [
+  {
+    name: "Salmon Creek",
+    image:
+      "https://images.fineartamerica.com/images-medium-large-5/1-a-view-of-the-ocean-from-inside-a-tent-rob-hammer.jpg"
+  },
+  {
+    name: "Granite Hill",
+    image:
+      "https://media.self.com/photos/5bcf8b52baeb4a5e6e13c899/4:3/w_1280,h_960,c_limit/tent-and-view.jpg"
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image: "https://i.ytimg.com/vi/vfkhlLnSq7o/maxresdefault.jpg"
+  }
+];
+
 app.get("/campgrounds", function(req, res) {
-  var campgrounds = [
-    {
-      name: "Salmon Creek",
-      image:
-        "https://images.fineartamerica.com/images-medium-large-5/1-a-view-of-the-ocean-from-inside-a-tent-rob-hammer.jpg"
-    },
-    {
-      name: "Granite Hill",
-      image:
-        "https://media.self.com/photos/5bcf8b52baeb4a5e6e13c899/4:3/w_1280,h_960,c_limit/tent-and-view.jpg"
-    },
-    {
-      name: "Mountain Goat's Rest",
-      image: "https://i.ytimg.com/vi/vfkhlLnSq7o/maxresdefault.jpg"
-    }
-  ];
   res.render("campgrounds", { campgrounds: campgrounds });
 });
 
 app.post("/campgrounds", function(req, res) {
-  res.send("You hit the POST route!");
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = { name: name, image: image };
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
 });
 
 app.get("/campgrounds/new", function(req, res) {
