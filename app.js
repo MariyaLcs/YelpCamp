7bd151e710801fcbc5c524ef1960200fef1df59f
Mariya Lucas <mariya.podosinova@gmail.com>
2020-01-28 13:55:58 +0000

    initial routes

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-  res.send("This will be the landing page!");
+  res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
+  var campgrounds = [
+    {
+      name: "Salmon Creek",
+      image:
+        "https://images.fineartamerica.com/images-medium-large-5/1-a-view-of-the-ocean-from-inside-a-tent-rob-hammer.jpg"
+    },
+    {
+      name: "Granite Hill",
+      image:
+        "https://media.self.com/photos/5bcf8b52baeb4a5e6e13c899/4:3/w_1280,h_960,c_limit/tent-and-view.jpg"
+    },
+    {
+      name: "Mountain Goat's Rest",
+      image: "https://i.ytimg.com/vi/vfkhlLnSq7o/maxresdefault.jpg"
+    }
+  ];
+  res.render("campgrounds", { campgrounds: campgrounds });
 });
 
 app.listen(process.env.PORT || 3000, function() {
