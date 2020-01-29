724c6f7618c653d8cf859e532b8e67be88cd1e11
Mariya Lucas <mariya.podosinova@gmail.com>
2020-01-29 20:46:51 +0000

    Campground Show Page

--- app.js
+++ app.js
@@ -16,23 +16,23 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-Campground.create(
-  {
-    name: "Salmon Creek",
-    image:
-      "https://images.fineartamerica.com/images-medium-large-5/1-a-view-of-the-ocean-from-inside-a-tent-rob-hammer.jpg",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-  },
-  function(err, campground) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("NEWLY CREATED CAMPGROUND: ");
-      console.log(campground);
-    }
-  }
-);
+// Campground.create(
+//   {
+//     name: "Salmon Creek",
+//     image:
+//       "https://images.fineartamerica.com/images-medium-large-5/1-a-view-of-the-ocean-from-inside-a-tent-rob-hammer.jpg",
+//     description:
+//       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+//   },
+//   function(err, campground) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("NEWLY CREATED CAMPGROUND: ");
+//       console.log(campground);
+//     }
+//   }
+// );
 
 app.get("/", function(req, res) {
   res.render("landing");
@@ -55,20 +55,23 @@ app.get("/", function(req, res) {
 //   }
 // ];
 
+//INDEX - SHOW ALL CAMPGROUNDS
 app.get("/campgrounds", function(req, res) {
   Campground.find({}, function(err, allCampgrounds) {
     if (err) {
       console.log(err);
     } else {
-      res.render("campgrounds", { campgrounds: allCampgrounds });
+      res.render("index", { campgrounds: allCampgrounds });
     }
   });
 });
 
+//CREATE - ADD NEW CAMPGROUND TO DB
 app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = { name: name, image: image };
+  var desc = req.body.description;
+  var newCampground = { name: name, image: image, description: desc };
   Campground.create(newCampground, function(err, newlyCreated) {
     if (err) {
       console.log(err);
@@ -82,8 +85,15 @@ app.get("/campgrounds/new", function(req, res) {
   res.render("new.ejs");
 });
 
+//SHOW - SHOWS MORE INFO ABOUT ONE CAMPGROUND
 app.get("/campgrounds/:id", function(req, res) {
-  res.send("THIS WILL BE THE SHOW PAGE ONE DAY!");
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("show", { campground: foundCampground });
+    }
+  });
 });
 
 app.listen(process.env.PORT || 3000, function() {
