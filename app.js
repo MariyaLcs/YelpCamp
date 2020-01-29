85db67ac437d385be460993a7a1896ffa5e5efe7
Mariya Lucas <mariya.podosinova@gmail.com>
2020-01-29 19:39:52 +0000

    Adding Mongoose

--- app.js
+++ app.js
@@ -1,33 +1,64 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Salmon Creek",
+//     image:
+//       "https://images.fineartamerica.com/images-medium-large-5/1-a-view-of-the-ocean-from-inside-a-tent-rob-hammer.jpg"
+//   },
+//   function(err, campground) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("NEWLY CREATED CAMPGROUND: ");
+//       console.log(campground);
+//     }
+//   }
+// );
 
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
-var campgrounds = [
-  {
-    name: "Salmon Creek",
-    image:
-      "https://images.fineartamerica.com/images-medium-large-5/1-a-view-of-the-ocean-from-inside-a-tent-rob-hammer.jpg"
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://media.self.com/photos/5bcf8b52baeb4a5e6e13c899/4:3/w_1280,h_960,c_limit/tent-and-view.jpg"
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image: "https://i.ytimg.com/vi/vfkhlLnSq7o/maxresdefault.jpg"
-  }
-];
+// var campgrounds = [
+//   {
+//     name: "Salmon Creek",
+//     image:
+//       "https://images.fineartamerica.com/images-medium-large-5/1-a-view-of-the-ocean-from-inside-a-tent-rob-hammer.jpg"
+//   },
+//   {
+//     name: "Granite Hill",
+//     image:
+//       "https://media.self.com/photos/5bcf8b52baeb4a5e6e13c899/4:3/w_1280,h_960,c_limit/tent-and-view.jpg"
+//   },
+//   {
+//     name: "Mountain Goat's Rest",
+//     image: "https://i.ytimg.com/vi/vfkhlLnSq7o/maxresdefault.jpg"
+//   }
+// ];
 
 app.get("/campgrounds", function(req, res) {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  Campground.find({}, function(err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds", { campgrounds: allCampgrounds });
+    }
+  });
 });
 
 app.post("/campgrounds", function(req, res) {
