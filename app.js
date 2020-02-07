890b2bd9f1ee46246bf1f3be1d4191a6d51ed2b5
Mariya Lucas <mariya.podosinova@gmail.com>
2020-02-07 15:23:42 +0000

    Install all packages needed for auth

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express = require("express"),
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.use("/public", express.static("public"));
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 app.get("/", function(req, res) {
