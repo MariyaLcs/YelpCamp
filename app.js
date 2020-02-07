ae8dbc2dbe956036d1512602cd7de136a40a720d
Mariya Lucas <mariya.podosinova@gmail.com>
2020-02-07 17:17:51 +0000

    Add login routes

--- app.js
+++ app.js
@@ -138,9 +138,19 @@ app.post("/register", function(req, res) {
 });
 
 //show login form
-app.get("/login", function(req, res){
-  res.render("login")
-})
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+//handling login logic
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }),
+  function(req, res) {}
+);
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server has started!");
