cf3f9984fc88039e6e0b256e32617668671071ee
Mariya Lucas <mariya.podosinova@gmail.com>
2020-02-07 17:10:55 +0000

    Add login template

--- app.js
+++ app.js
@@ -137,6 +137,11 @@ app.post("/register", function(req, res) {
   });
 });
 
+//show login form
+app.get("/login", function(req, res){
+  res.render("login")
+})
+
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server has started!");
 });
