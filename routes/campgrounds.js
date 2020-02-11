a0c4b97019756098689606fb85406f953ca1447f
Mariya Lucas <mariya.podosinova@gmail.com>
2020-02-11 13:50:16 +0000

    Prevent an unauthenticated user from creating a campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,7 +18,7 @@ router.get("/", function(req, res) {
 });
 
 //CREATE - ADD NEW CAMPGROUND TO DB
-router.post("/", function(req, res) {
+router.post("/", isLoggedIn, function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
@@ -35,7 +35,7 @@ router.post("/", function(req, res) {
 });
 
 //SHOW - show form to create new campground
-router.get("/new", function(req, res) {
+router.get("/new", isLoggedIn, function(req, res) {
   res.render("campgrounds/new");
 });
 
@@ -54,5 +54,14 @@ router.get("/:id", function(req, res) {
       }
     });
 });
+
+//Middleware
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect("/login");
+  }
+}
 
 module.exports = router;
