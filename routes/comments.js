b74f0604225628c045d45069796ddf5b3b9a0fb6
Mariya Lucas <mariya.podosinova@gmail.com>
2020-02-12 12:33:13 +0000

    Add Edit Route for comments

--- routes/comments.js
+++ routes/comments.js
@@ -40,6 +40,11 @@ router.post("/", isLoggedIn, function(req, res) {
     }
   });
 });
+
+router.get("/:comment_id/edit", function(req, res) {
+  res.send("Edit route comment!");
+});
+
 //Middleware
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
