6f8ded08729cf638db4425e2528b1dc07aa4137e
Mariya Lucas <mariya.podosinova@gmail.com>
2020-02-12 12:55:37 +0000

    Add Edit Template

--- routes/comments.js
+++ routes/comments.js
@@ -42,7 +42,16 @@ router.post("/", isLoggedIn, function(req, res) {
 });
 
 router.get("/:comment_id/edit", function(req, res) {
-  res.send("Edit route comment!");
+  Comment.findById(req.params.comment_id, function(err, foundComment) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", {
+        campground_id: req.params.id,
+        comment: foundComment
+      });
+    }
+  });
 });
 
 //Middleware
