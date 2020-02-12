9740d18cd63a02eae511ce502dbd40ddaa01ce95
Mariya Lucas <mariya.podosinova@gmail.com>
2020-02-12 11:32:28 +0000

    Add functionality for delete campgrounds only by owner

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -72,7 +72,7 @@ router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
 });
 
 //Update Campground Route
-router.put("/:id", function(req, res) {
+router.put("/:id", checkCampgroundOwnership, function(req, res) {
   //find and update the correct campground
   Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(
     err,
@@ -88,7 +88,7 @@ router.put("/:id", function(req, res) {
 });
 
 //Destroy Campground Route
-router.delete("/:id", function(req, res) {
+router.delete("/:id", checkCampgroundOwnership, function(req, res) {
   Campground.findByIdAndRemove(req.params.id, function(err) {
     if (err) {
       res.redirect("/campgrounds");
