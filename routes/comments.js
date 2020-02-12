e8af8ef573b4876771cd71afd2cdf5d18f2b1f2a
Mariya Lucas <mariya.podosinova@gmail.com>
2020-02-12 15:25:00 +0000

    Add functionalite where User can only edit his/her comments

--- routes/comments.js
+++ routes/comments.js
@@ -42,7 +42,7 @@ router.post("/", isLoggedIn, function(req, res) {
 });
 
 //Comment Edit Route
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
   Comment.findById(req.params.comment_id, function(err, foundComment) {
     if (err) {
       res.redirect("back");
@@ -79,6 +79,25 @@ router.delete("/:comment_id", function(req, res) {
     }
   });
 });
+
+function checkCommentOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function(err, foundComment) {
+      if (err) {
+        res.redirect("back");
+      } else {
+        //does user own the comment?
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+}
 
 //Middleware
 function isLoggedIn(req, res, next) {
